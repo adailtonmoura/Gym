980ca8f1be7d55e7f2ba761d18e633ee262a3529
Adailton Moura <54925262+adailtonmoura@users.noreply.github.com>
2020-02-16 13:56:07 -0300

    Estrutura Inicial

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer class="rodape bg-success">
+
+
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include_once('header.php'); ?>
+
+    <div class="principal">
+    <?php
+    $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 'home';
+
+        switch($pagina){
+            
+            case 'home':
+                include_once 'view/home.php';
+                break;
+
+            case 'emprestimo':
+                include_once 'view/emprestimo.php';
+                break;
+        }
+        ?>
+
+    </div>
+
+<?php include_once('footer.php');?>
